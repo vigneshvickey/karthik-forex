fec9f1ac49fb8624c7ac339229688aeb3fb46bee
vigneshvickey <vigneshvickey0@gmail.com>
2018-04-01 13:57:27 +0530

    new pages

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  //Set the carousel options
+  $('#quote-carousel').carousel({
+    pause: true,
+    interval: 4000,
+  });
+});
+$(document).ready(function() {
+$("#search").click(function(){
+	$("#package-holiday").hide();
+});
+});
