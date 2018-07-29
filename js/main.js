e9305e5b20a9f657ef173a343f555ec485daf498
vignesh <vigneshvickey0@gmail.com>
2018-07-29 16:17:00 +0530

    new

--- js/main.js
+++ js/main.js
@@ -10,3 +10,26 @@ $("#search").click(function(){
 	$("#package-holiday").hide();
 });
 });
+// $(function () {
+//   $(document).scroll(function () {
+// 	  var $nav = $(".navbar-fixed-top");
+// 	  $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+// 	});
+// });
+$(window).scroll(function() {    
+    var scroll = $(window).scrollTop();
+
+     //>=, not <=
+    if (scroll >= 100) {
+        //clearHeader, not clearheader - caps H
+        $(".navbar-trans ").addClass("darkHeader");
+    }
+    else {
+        $(".navbar-trans ").removeClass("darkHeader");
+    }
+});
+$('ul.nav li.dropdown').hover(function() {
+  $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(500);
+}, function() {
+  $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
+});
